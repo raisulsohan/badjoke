b376162d44a52b9df518e6be8ff8fb23e05a9026
Raisul Sohan <lettertosohan@gmail.com>
2024-01-08 02:04:10 +0600

    Update script.js
    added feature like showing previous jokes button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnEl = document.getElementById("btn");
-const jokeEL = document.getElementById("joke");
+const jokeEl = document.getElementById("joke");
+const prevJokeButtonEl = document.getElementById("prevJoke");
 
 const apiKey = "j0ZbcTnuuZHJCIVSzkqGcQ==eRx7cIFS5S8Bb56j";
 
@@ -11,28 +12,33 @@ const options = {
 }
 
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=";
+let currentJoke = "";
+let previousJoke = "";
 
 async function getJoke() {
-
     try {
-        jokeEL.innerText = "Updating...";
+        jokeEl.innerText = "Updating...";
         btnEl.disabled = true;
         btnEl.innerText = "Loading..."
         const response = await fetch(apiURL, options);
         const data = await response.json();
-    
         btnEl.disabled = false;
         btnEl.innerText = "Tell me a joke again!"
-    
-        jokeEL.innerText = data[0].joke;        
+        previousJoke = currentJoke;
+        currentJoke = data[0].joke;
+        jokeEl.innerText = currentJoke;
     } catch (error) {
-        jokeEL.innerText = "An error happened, try again!";
+        jokeEl.innerText = "An error happened, try again!";
         btnEl.disabled = false;
         btnEl.innerText = "Tell me a joke again!"
         console.log(error);
     }
-
-
 }
 
-btnEl.addEventListener("click", getJoke)
+function showPreviousJoke() {
+    // Display the previous joke directly in the "joke" element
+    jokeEl.innerText = "Previous Joke: " + previousJoke;
+}
+
+btnEl.addEventListener("click", getJoke);
+prevJokeButtonEl.addEventListener("click", showPreviousJoke);
